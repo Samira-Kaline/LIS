410d439938cda8756953531ee00e06cce3a67ee5
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2022-02-18 16:27:15 -0300

    Código Comentado
    Código Comentado

--- CafeAppLis/js/model/dataset.js
+++ CafeAppLis/js/model/dataset.js
@@ -1,32 +1,53 @@
+//Criação de um constante do tipo Json
 const dataset = [
-  {
+  //Bloco de Json
+  { 
+    //Elemento chave e valor
     id: 1,
+    //Elemento chave e valor
     name: 'Hambúrguer',
+    //Elemento chave e valor
     image: 'images/hamburguer.jpg',
+    //Elemento chave e valor
     description:
       'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
   },
+  //Bloco de Json
   {
+    //Elemento chave e valor
     id: 2,
+    //Elemento chave e valor
     name: 'Sanduíche',
+    //Elemento chave e valor
     image: 'images/sanduiche.jpg',
+    //Elemento chave e valor
     description:
       'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
   },
+  //Bloco de Json
   {
+    //Elemento chave e valor
     id: 3,
+    //Elemento chave e valor
     name: 'Café',
+    //Elemento chave e valor
     image: 'images/cafe.jpg',
+    //Elemento chave e valor
     description:
       'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
   },
+  //Bloco de Json
   {
+    //Elemento chave e valor
     id: 4,
+    //Elemento chave e valor
     name: 'Suco',
+    //Elemento chave e valor
     image: 'images/suco.jpg',
+    //Elemento chave e valor
     description:
       'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.',
   },
 ];
-
+//Retornando o Json
 export default dataset;
